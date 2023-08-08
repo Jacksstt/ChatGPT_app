692f35f566e95e7325744fcb0f99bf3ad54fc187
Jack.tt <shibuyaryuukyou@TatsunariShibuyasMacBook-Air.local>
2023-08-08 20:32:52 +0900

    u

--- 00_my_first_app.py
+++ 00_my_first_app.py
@@ -6,14 +6,16 @@ import os
 # OpenAIのAPIキーを設定
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
-
 def get_gpt3_response(prompt):
-    response = openai.Completion.create(
-      engine="gpt-3.5-turbo",  # engineをgpt-3.5-turboに変更
-      prompt=prompt,
-      max_tokens=150
+    response = openai.ChatCompletion.create(
+      model="gpt-3.5-turbo",
+      messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": prompt}
+        ]
     )
-    return response.choices[0].text.strip()
+    return response.choices[0].message['content'].strip()
+
 
 st.title('ChatGPTとの音声会話')
 
