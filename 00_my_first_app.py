05b47e118d33517d94ec1bc2b3e9a458be40d536
Jack.tt <shibuyaryuukyou@TatsunariShibuyasMacBook-Air.local>
2023-08-09 00:38:23 +0900

    u

--- 00_my_first_app.py
+++ 00_my_first_app.py
@@ -17,15 +17,32 @@ def get_gpt3_response(prompt):
     )
     return response.choices[0].message['content'].strip()
 
-def get_image_from_duckduckgo(query):
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+# CSS for the chat bubbles
+chat_style = """
+<style>
+    .chat-bubble {
+        padding: 10px;
+        border-radius: 15px;
+        margin: 5px 0;
     }
-    url = f"https://duckduckgo.com/?q={query}&t=h_&iax=images&ia=images"
-    response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.content, "html.parser")
-    img_tag = soup.find("img", {"class": "tile--img__img"})
-    return img_tag["src"] if img_tag else None
+    .user {
+        background-color: #e1ffc7;
+        align-self: flex-start;
+    }
+    .chatgpt {
+        background-color: #c7c7ff;
+        align-self: flex-end;
+    }
+    .chat-container {
+        display: flex;
+        flex-direction: column;
+        width: 50%;
+        margin: auto;
+    }
+</style>
+"""
+
+st.markdown(chat_style, unsafe_allow_html=True)
 
 st.title('ChatGPTとの音声会話')
 
@@ -44,12 +61,13 @@ if user_input:
     tts = gTTS(text=response, lang='ja')
     tts.save("response.mp3")
 
-        # 会話の履歴を表示
-    for item in conversation_history:
-        if item["role"] == "user":
-            st.write(f"あなた: {item['content']}")
-        else:
-            st.write(f"ChatGPT: {item['content']}")
+    # 会話の履歴を表示
+    with st.markdown('<div class="chat-container">', unsafe_allow_html=True):
+        for item in conversation_history:
+            if item["role"] == "user":
+                st.markdown(f'<div class="chat-bubble user">{item["content"]}</div>', unsafe_allow_html=True)
+            else:
+                st.markdown(f'<div class="chat-bubble chatgpt">{item["content"]}</div>', unsafe_allow_html=True)
     
     st.write(f"ChatGPT: {response}")
     st.audio("response.mp3")
