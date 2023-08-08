e2c83fbe5755de760ff4a4d2ab799a6eb97a940d
Jack.tt <shibuyaryuukyou@TatsunariShibuyasMacBook-Air.local>
2023-08-08 23:08:51 +0900

    u

--- 00_my_first_app.py
+++ 00_my_first_app.py
@@ -2,15 +2,16 @@ import streamlit as st
 from gtts import gTTS
 import openai
 import os
+from bs4 import BeautifulSoup
+import requests
 
-# OpenAIのAPIキーを設定
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 def get_gpt3_response(prompt):
     response = openai.ChatCompletion.create(
       model="gpt-3.5-turbo",
       messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "system", "content": "You are an assistant that provides hints and feedback on user's questions, especially if they sound like they're from children. Do not provide direct answers, but guide them towards finding the answer themselves."},
             {"role": "user", "content": prompt}
         ]
     )
@@ -28,46 +29,17 @@ def get_image_from_duckduckgo(query):
 
 st.title('ChatGPTとの音声会話')
 
-# 音声入力のためのHTMLを追加
-html = """
-    <div>
-        <button onclick="startDictation()" type="button">音声入力開始</button>
-        <textarea id="result" rows="10" cols="50"></textarea>
-    </div>
-    <script>
-        function startDictation() {
-            if (window.hasOwnProperty('webkitSpeechRecognition')) {
-                var recognition = new webkitSpeechRecognition();
-                recognition.continuous = false;
-                recognition.interimResults = false;
-                recognition.lang = "ja-JP";  # 日本語に設定
-                recognition.start();
-                recognition.onresult = function(e) {
-                    document.getElementById('result').value = e.results[0][0].transcript;
-                    recognition.stop();
-                };
-                recognition.onerror = function(e) {
-                    recognition.stop();
-                }
-            }
-        }
-    </script>
-"""
-
-st.markdown(html, unsafe_allow_html=True)
-
-# ユーザーからの音声入力を取得
-user_input = st.text_area("あなたの質問を入力してください:")
+user_input = st.text_input("あなたの質問を入力してください:")
 
 if user_input:
-    # ChatGPTからの応答を取得
     response = get_gpt3_response(user_input)
     
-    # 応答を音声に変換
     tts = gTTS(text=response, lang='ja')
     tts.save("response.mp3")
     
-    # 応答を表示と音声再生
     st.write(f"ChatGPT: {response}")
     st.audio("response.mp3")
     
+    image_url = get_image_from_duckduckgo(user_input)
+    if image_url:
+        st.image(image_url, caption="関連画像", use_column_width=True)
